9a1711831573bf54d92197ca284592a9b592c5a6
Nikolay37731 <anikolai3773@gmail.com>
2024-11-15 09:36:24 +0200

    Solving problem: Max and min element

---
+++ Stacks and queues/03. Maximum_and_minimum_element .cs
@@ -0,0 +1,30 @@
+using System.Linq;
+
+int queries = int.Parse(Console.ReadLine());
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < queries; i++)
+{
+	string[] tokens = Console.ReadLine().Split().ToArray();
+	int command = int.Parse(tokens[0]);
+
+	if (tokens.Length > 1)
+	{
+		int num = int.Parse(tokens[1]);
+		stack.Push(num);
+	}
+	else if (command == 2)
+	{
+		stack.Pop();
+	}
+	else if (command == 3 && stack.Any())
+	{
+		Console.WriteLine(stack.Max());
+	}
+	else if (command == 4 && stack.Any())
+	{
+		Console.WriteLine(stack.Min());
+	}
+}
+
+Console.WriteLine(String.Join(", ", stack));
