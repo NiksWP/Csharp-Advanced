6784732878ff505fa0bbf673328da061f37e1e84
Nikolay37731 <anikolai3773@gmail.com>
2024-12-01 20:52:36 +0200

    Solving problem

---
+++ Exam prep/DataCenter/Rack.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace DataCenter
+{
+	public class Rack
+	{
+		public int Slots { get; set; }
+		public List<Server> Servers { get; set; }
+		public int GetCount => Servers.Count;
+		public Rack(int slots)
+		{
+			this.Slots = slots;
+			this.Servers = new List<Server>();
+		}
+
+		public void AddServer(Server server)
+		{
+			if (this.GetCount < this.Slots && !this.Servers.Any(x => x.SerialNumber == server.SerialNumber))
+			{
+				this.Servers.Add(server);
+			}
+		}
+		public bool RemoveServer (string server)
+		{
+			Server toRemove = Servers.Where(x => x.SerialNumber == server).FirstOrDefault();
+			if (toRemove != null)
+			{
+				Servers.Remove(toRemove);
+				return true;
+			}
+			return false;
+		}
+		public string GetHighestPowerUsage()
+		{
+			Server greatest = Servers.OrderByDescending(x => x.PowerUsage).First();
+			return greatest.ToString();
+		}
+
+		public int GetTotalCapacity()
+		{
+			return Servers.Sum(x => x.Capacity);
+		}
+
+		public string DeviceManager()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"{this.GetCount} servers operating:");
+			foreach (var server in Servers)
+			{
+				sb.AppendLine(server.ToString().TrimEnd());
+			}
+
+			return sb.ToString().TrimEnd();
+		}
+	}
+}
